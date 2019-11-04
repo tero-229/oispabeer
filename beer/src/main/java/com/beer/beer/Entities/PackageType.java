647ac31b58e01054ca3c38dc0ce30030dee1c063
malin <malin.e.kraft@gmail.com>
2019-11-04 15:35:40 +0200

    fixed bugs

--- beer/src/main/java/com/beer/beer/Entities/PackageType.java
+++ beer/src/main/java/com/beer/beer/Entities/PackageType.java
@@ -1,9 +1,9 @@
 package com.beer.beer.Entities;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "PackageType")
 public class PackageType {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
