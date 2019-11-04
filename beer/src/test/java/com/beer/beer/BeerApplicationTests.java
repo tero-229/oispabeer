fba7b451c16cb588cc9c8f85fed49ebe679f1e36
rissanssi <anssirissa@gmail.com>
2019-11-04 11:44:50 +0200

    Tästä liikenteeseen

---
+++ beer/src/test/java/com/beer/beer/BeerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beer.beer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
