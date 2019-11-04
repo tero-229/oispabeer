a1c1be778e39b3f79c581e49a6315dc73f97f44c
rissanssi <anssirissa@gmail.com>
2019-11-04 14:23:02 +0200

    Entitiesejä päivitelty siihen malliin, että kaikki löytyypi...

--- beer/src/main/java/com/beer/beer/Entities/Ingredients.java
+++ beer/src/main/java/com/beer/beer/Entities/Ingredients.java
@@ -1,4 +1,16 @@
 package com.beer.beer.Entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ingredients")
 public class Ingredients {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private long BeerId;
+    private String name;
+
+
 }
--- beer/src/main/java/com/beer/beer/Entities/PackageType.java
+++ beer/src/main/java/com/beer/beer/Entities/PackageType.java
@@ -1,4 +1,18 @@
 package com.beer.beer.Entities;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class PackageType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private long beerId;
+    private String type;
+    private int size;
+    private double price;
+
+
 }
