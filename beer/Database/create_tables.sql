17cb114880ae1f676a03b92ff835dcfd6e8aff8a
Tero <tero.makila@hotmail.com>
2019-11-04 13:38:52 +0200

    Brewery table lisätty

---
+++ beer/Database/create_tables.sql
@@ -0,0 +1,8 @@
+CREATE TABLE brewery (
+    id                  SERIAL              PRIMARY KEY,
+    name                VARCHAR(30)         NOT NULL,
+    address             CHAR(25),
+    country             VARCHAR(25),
+    homepage            VARCHAR(25),
+    foundation_year     INT,
+)
