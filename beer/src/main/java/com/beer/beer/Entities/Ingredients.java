414f5705933a09bad92859bd332d3465d67fab99
rissanssi <anssirissa@gmail.com>
2019-11-04 14:28:01 +0200

    Entitiesejä päivitelty siihen malliin, että kaikki pitäis oleman ml. konstukrorit, getterit ja setterit...

--- beer/src/main/java/com/beer/beer/Entities/Ingredients.java
+++ beer/src/main/java/com/beer/beer/Entities/Ingredients.java
@@ -12,5 +12,29 @@ public class Ingredients {
     private long BeerId;
     private String name;
 
+    public Ingredients() { }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getBeerId() {
+        return BeerId;
+    }
+
+    public void setBeerId(long beerId) {
+        BeerId = beerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- beer/src/main/java/com/beer/beer/Entities/PackageType.java
+++ beer/src/main/java/com/beer/beer/Entities/PackageType.java
@@ -14,5 +14,45 @@ public class PackageType {
     private int size;
     private double price;
 
+    public PackageType() { }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getBeerId() {
+        return beerId;
+    }
+
+    public void setBeerId(long beerId) {
+        this.beerId = beerId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
