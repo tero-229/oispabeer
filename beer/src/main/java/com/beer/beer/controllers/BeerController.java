b346789bd534d158649ab8957c880b2c977e5c6a
malin <malin.e.kraft@gmail.com>
2019-11-04 13:20:33 +0200

    Lisätty repositories ja controller packages

---
+++ beer/src/main/java/com/beer/beer/controllers/BeerController.java
@@ -0,0 +1,12 @@
+package com.beer.beer.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/beer")
+public class BeerController {
+
+
+}
